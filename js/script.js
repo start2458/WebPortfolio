2c4f5ca0fa895ec86ee71710a02bf6ac869068c3
JHP._.05 <start2458@gmail.com>
2022-11-23 15:54:29 +0900

    지금 고개들면 넌 떠나갈 텐데

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ $(function(){
         let slidePosition = currentIndex * (-1000)+"px";
         $(".slideList").animate({left:slidePosition},400)
     },3000);
+});
 
     $(function(){
         $(".nav>ul>li").mouseover(function(){
@@ -21,4 +22,29 @@ $(function(){
             $(".nav>ul>li>ul").stop().slideUp(200);
         })
     });
+
+//sokcho_slide
+let s_slide_position = 0;
+let cnt = 0;
+$(".lbtn").click(function(){
+    cnt--;
+    if(cnt < 0){
+        cnt = $(".s_slideImg").length - 1;
+        s_slide_position = cnt * (-1000)+"px";
+        $(".sokcho_slide").animate({marginLeft:s_slide_position}, 400);
+    }else{    
+        s_slide_position = cnt * (-1000)+"px";
+        $(".sokcho_slide").animate({marginLeft:s_slide_position}, 400);
+    }
+});
+$(".rbtn").click(function(){
+    cnt++;
+    if(cnt > 2){
+        cnt = 0;
+        s_slide_position = cnt * (-1000)+"px";
+        $(".sokcho_slide").animate({marginLeft:s_slide_position}, 400);
+    }else{    
+        s_slide_position = cnt * (-1000)+"px";
+        $(".sokcho_slide").animate({marginLeft:s_slide_position}, 400);
+    }
 });
